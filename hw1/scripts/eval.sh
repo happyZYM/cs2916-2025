26b5eedaa78c307706c1753fcaeadc43a80ec4ba
ZhunagYumin <zymx@pm.me>
2025-04-27 16:49:54 +0800

    confirmed

--- hw1/scripts/eval.sh
+++ hw1/scripts/eval.sh
@@ -2,5 +2,5 @@ export HOST_IP=$MASTER_ADDR
 export NCCL_CUMEM_ENABLE=0
 export CUDA_VISIBLE_DEVICES=0
 
-ckpt_path=../ckpts/sft_shortcot2
+ckpt_path=../ckpts/sft_explore_512_2e-05/epoch_6
 python src/evaluation/evaluation.py --model_path $ckpt_path --max_tokens 16384
